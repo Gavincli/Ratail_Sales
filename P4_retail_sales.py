6ed1232ee1a3c063c78dadde3c3070fcba49abea
thomas-apke <tapke@byu.edu>
2025-04-14 20:39:14 -0600

    Update P4_retail_sales.py

--- P4_retail_sales.py
+++ P4_retail_sales.py
@@ -2,3 +2,8 @@
 # Assume you are given a sample of sale data from an online retailer in the form of an excel file.
 # The retailer wants your team to test how they could transfer their data into a postgres database
 # and read data programmatically back from the database.
+
+import sqlalchemy
+import pandas as pd
+import matplotlib.pyplot as plot
+import psycopg2
